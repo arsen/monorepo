4ae7ee0c7f78ff12121d4459a8abd23c1aee0f51
Arsen Ghazaryan <arsen.ghazaryan@gmail.com>
2025-11-18 18:34:57 -0800

    remove web-bk

--- apps/web-bk/next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@everdesk/ui"],
-}
-
-export default nextConfig
--- apps/web-bk/postcss.config.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "@everdesk/ui/postcss.config";
