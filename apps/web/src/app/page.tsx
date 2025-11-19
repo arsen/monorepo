9ae67ba526e5a045440d40cdee8934ee33cf23d2
Arsen Ghazaryan <arsen.ghazaryan@gmail.com>
2025-11-18 19:13:38 -0800

    adding shared types dependency to next.js app

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,13 @@
+import { Example } from "@everdesk/types";
 import { Button } from "@everdesk/ui/components/button";
 
 export default function Home() { 
+  const example = Example.parse({
+    id: "1",
+    name: "Example",
+    createdAt: new Date(),
+  });
+  console.log(example);
   return (
     <div className="flex flex-col gap-4 min-h-screen items-center justify-center">
       <h1>Hello World</h1>
